aa346d8bd9f5b2ebea9eea2106a32a0ddef453c0
gabe rankin <gabe.rankin@gmail.com>
2013-12-02 15:05:36 -0500

    get file uploads working in flask-test

--- flask-test/TODO.md
+++ flask-test/TODO.md
@@ -1,3 +1,8 @@
+Dec 02 2013
+-----------
+ + make file uploads do stuff?  
+ + I don't know.  I'm f*&%ing around really
+
 Oct 10 2013
 -----------
  + make it display the posts that have been added to the database
--- flask-test/main.py
+++ flask-test/main.py
@@ -1,11 +1,17 @@
 from flask import abort, flash, Flask, redirect, render_template, request, url_for
 from db import Post
-import functions as helpers, datetime
+from werkzeug import secure_filename
+import functions as helpers, datetime, os
 
 app = Flask(__name__)
 app.secret_key = 'this is my secret key.  i have shoes that are not good for dancing'
 app.jinja_env.globals.update(helpers = helpers) #register the helper functions.
 
+UPLOAD_FOLDER = '/Users/gaberankin/Sites/random-crap/flask-test/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'asf'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/", defaults={'page': 1})
 @app.route("/<int:page>")
@@ -51,6 +57,28 @@ def update_post(post_id):
 			flash("Post successfully updated", "success")
 			return redirect(url_for('update_post', post_id = p.id))
 	return render_template('post/update.html', error = error, post = p)
+
+def allowed_file(filename):
+	return '.' in filename and \
+		   filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/uploadtest', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		file = request.files['file']
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('uploaded_file', filename=filename))
+	return '''
+	<!doctype html>
+	<title>Upload new File</title>
+	<h1>Upload new File</h1>
+	<form action="" method=post enctype=multipart/form-data>
+	  <p><input type=file name=file>
+		 <input type=submit value=Upload></p>
+	</form>
+	'''
 
 @app.errorhandler(404)
 def page_not_found(error):
