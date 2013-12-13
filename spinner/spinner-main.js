7ae39b366e2fbf99cbb449e6ce0837943a1192e2
gabe rankin <gabe.rankin@gmail.com>
2013-12-13 14:07:33 -0500

    messing around with javascript that behaves like a 'spinner', where the input field doesn't allow non-numeric input, and pressing the arrow keys will increase or decrease the size

---
+++ spinner/spinner-main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$('#spinner').keydown(arrowKeyUpdate).keypress(checkNumeric);
+});
+
+function checkNumeric(e)
+{
+	'use strict';
+	var k = e.which;
+
+	if(k < 48 || k > 57)
+		e.preventDefault();
+}
+
+function arrowKeyUpdate(e)
+{
+	'use strict';
+	var k = e.which;
+
+	if(k == 38)
+	{
+		var v = $(this).val() * 1;
+		$(this).val(v + 1);
+		e.preventDefault();
+	}
+	else if(k == 40)
+	{
+		var v = $(this).val() * 1;
+		$(this).val(v - 1);
+		e.preventDefault();
+	}
+}
