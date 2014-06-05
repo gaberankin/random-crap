b2051c74c6eca6487920a782193bcaf20751c398
gabe rankin <gabe.rankin@gmail.com>
2014-06-05 15:51:00 -0400

    cleanup

--- flask-test/functions.py
+++
@@ -1,8 +0,0 @@
-import pytz
-import datetime
-
-def date_format(dateobj, format = None, tz = 'America/New_York'):
-	if format != None:
-		return pytz.utc.localize(dateobj).astimezone(pytz.timezone(tz)).strftime(format)
-	else:
-		return pytz.utc.localize(dateobj).astimezone(pytz.timezone(tz))
--- iframe-test/TODO.md
+++
@@ -1,5 +0,0 @@
-Aug 23 2013
------------
- + add support for IE10's MessageChannel
- + wrap into simple js api for iframe or parent window.
-   + I don't know.
