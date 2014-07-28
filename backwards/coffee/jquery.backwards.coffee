4a7a65dfb8d03bab16a70505fc333561c6a0d50f
gabe rankin <gabe.rankin@gmail.com>
2014-07-28 17:02:32 -0400

    backwards.js is working in chrome.  Haven't tested it in other browsers yet though

--- backwards/coffee/jquery.backwards.coffee
+++ backwards/coffee/jquery.backwards.coffee
@@ -16,25 +16,42 @@
 			return
 		).css({'color':'transparent'}).each(() ->
 			$me = $(this)
+			left = (($me.outerWidth() - $me.width()) / 2) + parseInt($me.css('margin-left'))
+			top = (($me.outerHeight() - $me.height()) / 2) + parseInt($me.css('margin-top'))
 			$me.wrap('<div style="position:relative; display:inline-block;" class="backwards-wrap"></div>')
 			$parent = $me.parent('.backwards-wrap')
 			$text = $('<div class="backwards-text"></div>').appendTo($parent).css({
-				padding: $me.css('padding')
+				top: top
+				left: left
 				zIndex: 2
 				position: 'absolute'
-				maxWidth: $me.outerWidth()
-				height: $me.outerHeight()
+				maxWidth: $me.width()
+				height: $me.height()
 				overflow: 'hidden'
+				whiteSpace: 'nowrap'
 			}).click(()->
 				$me.get(0).focus()
+				return
 			)
+			if $me.css('font')
+				$text.css('font', $me.css('font'))
+			else
+				if $me.css('font-family')
+					$text.css('font-family', $me.css('font-family'))
+				if $me.css('font-size')
+					$text.css('font-size', $me.css('font-size'))
+			if $me.css('font-weight')
+				$text.css('font-weight', $me.css('font-weight'))
+			if $me.css('font-style')
+				$text.css('font-style', $me.css('font-style'))
+
 			$me.css({
 				position: 'absolute',
 				zIndex: 1
 			})
 			$parent.css({
 				width: $me.outerWidth()
-				height: $me.outerHeight()
+				# height: $me.outerHeight()
 			})
 			return
 		)
--- backwards/js/jquery.backwards.js
+++ backwards/js/jquery.backwards.js
@@ -18,27 +18,46 @@
     }).css({
       'color': 'transparent'
     }).each(function() {
-      var $me, $parent, $text;
+      var $me, $parent, $text, left, top;
       $me = $(this);
+      left = (($me.outerWidth() - $me.width()) / 2) + parseInt($me.css('margin-left'));
+      top = (($me.outerHeight() - $me.height()) / 2) + parseInt($me.css('margin-top'));
       $me.wrap('<div style="position:relative; display:inline-block;" class="backwards-wrap"></div>');
       $parent = $me.parent('.backwards-wrap');
       $text = $('<div class="backwards-text"></div>').appendTo($parent).css({
-        padding: $me.css('padding'),
+        top: top,
+        left: left,
         zIndex: 2,
         position: 'absolute',
-        maxWidth: $me.outerWidth(),
-        height: $me.outerHeight(),
-        overflow: 'hidden'
+        maxWidth: $me.width(),
+        height: $me.height(),
+        overflow: 'hidden',
+        whiteSpace: 'nowrap'
       }).click(function() {
-        return $me.get(0).focus();
+        $me.get(0).focus();
       });
+      if ($me.css('font')) {
+        $text.css('font', $me.css('font'));
+      } else {
+        if ($me.css('font-family')) {
+          $text.css('font-family', $me.css('font-family'));
+        }
+        if ($me.css('font-size')) {
+          $text.css('font-size', $me.css('font-size'));
+        }
+      }
+      if ($me.css('font-weight')) {
+        $text.css('font-weight', $me.css('font-weight'));
+      }
+      if ($me.css('font-style')) {
+        $text.css('font-style', $me.css('font-style'));
+      }
       $me.css({
         position: 'absolute',
         zIndex: 1
       });
       $parent.css({
-        width: $me.outerWidth(),
-        height: $me.outerHeight()
+        width: $me.outerWidth()
       });
     });
   };
