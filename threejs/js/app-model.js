c6327a707c12a4ac1aa7cca9ab82c707f0d4fa7f
gabe rankin <gabe.rankin@gmail.com>
2014-09-22 16:10:25 -0400

    fix stupid light source because i am stupid

--- threejs/js/app-model.js
+++ threejs/js/app-model.js
@@ -25,17 +25,18 @@ var setup = function(){
 	$container.append(renderer.domElement);
 	clock = new THREE.Clock();
 
-	var light = new THREE.AmbientLight(0xfffff3, 0.8);
-	var light2 = new THREE.PointLight( 0xff0000, 1, 800 );
-	light2.position.set(new THREE.Vector3( 0, 150, 150 ))
+	// var light = new THREE.AmbientLight(0xfffff3, 0.8);
+	var light2 = new THREE.PointLight( 0xffffff, 4, 800 );
+	light2.position.set( 150, 150, 150 );
+	light2.castShadow = true;
 	// var shadowCaster = new THREE.SpotLight(0xfffff3, 0.8);
 	// shadowCaster.position.set(camera.position);
 	// shadowCaster.castShadow = true;
 	// shadowCaster.onlyShadow = true;
 	// shadowCaster.distance = 400;
-	// shadowCaster.target
-	// light.position.set(100 * (3/4),200 * (3/4),100 * (3/4));
-	scene.add(light);
+	// // shadowCaster.target
+	// // light.position.set(100 * (3/4),200 * (3/4),100 * (3/4));
+	// scene.add(shadowCaster);
 	scene.add(light2);
 
 	loader.load('./js/dumb-box.js', function (geometry, materials) {
@@ -43,6 +44,18 @@ var setup = function(){
 		// skinnedMesh.position.y = 50;
 		skinnedMesh.scale.set(15, 15, 15);
 		scene.add(skinnedMesh);
+
+		var x_line_geometry = new THREE.Geometry();
+		x_line_geometry.vertices.push(new THREE.Vector3( 50, 0, 0 ), new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, 0 ) );
+		scene.add( new THREE.Line( x_line_geometry, new THREE.LineBasicMaterial({ color: 0x0000ff }) ) );
+
+		var y_line_geometry = new THREE.Geometry();
+		y_line_geometry.vertices.push(new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 50, 0 ), new THREE.Vector3( 0, 0, 0 ) );
+		scene.add( new THREE.Line( y_line_geometry, new THREE.LineBasicMaterial({ color: 0xffff00 }) ) );
+
+		var z_line_geometry = new THREE.Geometry();
+		z_line_geometry.vertices.push(new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, 50 ) );
+		scene.add( new THREE.Line( z_line_geometry, new THREE.LineBasicMaterial({ color: 0xff0000 }) ) );
 
 		setInterval(function(){
 			skinnedMesh.rotateY(clock.getDelta());
